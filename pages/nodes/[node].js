b0adaebf3b2e8cb48df71759f5d337382bcac9c7
Kerry Wang <kerrywang369@gmail.com>
2022-12-08 19:31:13 -0800

    working fine

--- pages/nodes/[node].js
+++ pages/nodes/[node].js
@@ -5,8 +5,10 @@
 import fs from 'fs'
 import path from 'path'
 import getAllFilesRecursively from '../../lib/utils/files'
-import Graph from '../../lib/graph'
-import ReactMarkdown from 'react-markdown'
+import Graph from '../../lib/Graph'
+// import ReactMarkdown from 'react-markdown'
+import MarkdownRender from '../../lib/MarkdownRender'
+import { useRouter } from 'next/router'
 
 const root = process.cwd()
 const prefixPaths = path.join(root, "data", "Knowledge");
@@ -33,17 +35,27 @@ export async function getStaticProps({ params }) {
   // return { props: { title: params.node, content: posts[10].urlEnding.toString()} }
   post = posts.filter((post) => post.urlEnding === params.node)[0];
   if (!post) {
-    return { props: { title: params.node, content: "No content"} };
+    return { props: { title: params.node, content: "No content", backlinks: []} };
   }
-  return { props: { title: params.node, content: post.content} }
+  return { props: { title: post.title, content: post.content, backlinks: post.backlinks} }
 }
 
-export default function Node({ title, content }) {
+export default function Node({ title, content, backlinks}) {
+  const router = useRouter()
+  const fullPath = router.pathname;
+  const urlPrefix = fullPath.substring(0, fullPath.lastIndexOf("/")) + "/";
   return (
     <>
       <h1>{title}</h1>
-      {/* <text>{content}</text> */}
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <MarkdownRender>{content}</MarkdownRender>
+      <h2>Backlinks</h2>
+      <ul>
+        {backlinks.map((backlink) => (
+          <a href={urlPrefix + backlink} key={backlink}>
+            <li>{backlink.replace(/_/g, " ")}</li>
+          </a>
+        ))}
+      </ul>
     </>
   )
 }
